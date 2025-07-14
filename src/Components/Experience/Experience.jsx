8a128a9f66a2eb96a92171b5f8a34ce163ae1039
Ankita Paul <164400693+Ankitapaul1234@users.noreply.github.com>
2025-07-14 21:06:26 +0530

    Update Experience.jsx

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -446,6 +446,8 @@ const skills = [
   { icon: <FaJava color="#007396" size={50} />, name: "Java" },
   { icon: <FaPython color="#3776AB" size={50} />, name: "Python" },
   { icon: <FaDatabase color="#4479A1" size={50} />, name: "SQL" },
+ { icon: <SiMysql color="#00758F" size={50} />, name: "MySQL" },
+  { icon: <DiOracle color="#F80000" size={50} />, name: "PL/SQL" },
   { icon: <SiNextdotjs color="white" size={50} />, name: "Next.js" },
   { icon: <FaReact color="#61DAFB" size={50} />, name: "React.js" },
   { icon: <SiTailwindcss color="#38B2AC" size={50} />, name: "Tailwind" },
