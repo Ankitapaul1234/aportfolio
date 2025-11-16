8a4e07739d8af7872e2df3d290cf684dacb2d06b
Ankita Paul <164400693+Ankitapaul1234@users.noreply.github.com>
2025-11-16 17:39:56 +0530

    Update Experience.jsx

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -484,7 +484,7 @@ const Experience = () => {
   return (
     <div id="Experience" className="p-10 md:p-24">
       <motion.h1
-        className="text-2xl md:text-4xl font-bold mb-8 text-[#d4ac0d]"
+        className="text-2xl md:text-4xl font-bold mb-8 text-white"
         initial={{ opacity: 0, y: -50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
