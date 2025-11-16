eee85a979e9e44d9616aa98bdab750c1789c3a9a
Ankita Paul <164400693+Ankitapaul1234@users.noreply.github.com>
2025-11-16 16:46:59 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -72,10 +72,16 @@ const Home = () => {
           Welcome to my portfolio! Take a look at my work — let's build something awesome together.
         </p> */}
 
-       <p className="text-sm md:text-2xl tracking-tight mb-6 
+       {/* <p className="text-sm md:text-2xl tracking-tight mb-6 
                text-gray-300 font-light leading-relaxed">
   Welcome to my portfolio! Take a look at my work — let's build something awesome together.
+</p> */}
+       <p className="text-sm md:text-2xl tracking-tight mb-6 
+               text-gray-300 font-light leading-relaxed"
+   style={{ fontFamily: "'Dancing Script', cursive" }}>
+  Welcome to my portfolio! Take a look at my work — let's build something awesome together.
 </p>
+
 
         {/* <button className="mt-5 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-6 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#2a79e6]">
           Contact Me
