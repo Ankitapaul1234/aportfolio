ae2e706763ea2f786b08a4cd654b26e71c6d8922
Ankita Paul <164400693+Ankitapaul1234@users.noreply.github.com>
2025-11-16 13:32:59 +0530

    Clean up commented code in Home.jsx
    Removed commented-out code for the avatar image.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -82,17 +82,13 @@ const Home = () => {
       </div>
 
       {/* Right image */}
-      {/* <div className="md:w-2/4 flex justify-center md:justify-end mt-10 md:mt-0">
+       <div className="md:w-2/4 flex justify-center md:justify-end mt-10 md:mt-0">
         <img
           src={avatarImg}
           alt="Ankita Paul"
           className="block mx-auto sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl animate-float"
-        /> */}
-
-     
-  
-
-    {/* </div>  */}
+        /> 
+       </div>  
 
      {/* <div className="md:w-2/4 flex justify-center md:justify-end mt-10 md:mt-0">
   <img
@@ -102,13 +98,7 @@ const Home = () => {
   />
 </div> */}
 
-     <div className="md:w-2/4 flex justify-center md:justify-end mt-10 md:mt-0">
-  <img
-    src={avatarImg}
-    alt="Ankita Paul"
-    className="rounded-full object-cover w-50 h-50"
-  />
-</div>
+
 
 
 
